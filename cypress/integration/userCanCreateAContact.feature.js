07f8b28eabff082a02928160604c11c064953871
allezz <aerbay@gmail.com>
2020-03-29 09:35:41 +0200

    Add #contact-list test

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -3,11 +3,22 @@ describe("user can create a contact", () => {
         cy.visit("http://192.168.0.3:3001")
         cy.get("#add-contact").click()
         cy.get("#name").type("Ali")
-		cy.get("#email").type("aerbay@gmail.com")
+		cy.get("#email").type("test@test.com")
 		cy.get("#phone").type("0707666999")
 		cy.get("#company").type("Craft Academy")
 		cy.get("#notes").type("Beginner coder")
         cy.get("#twitter").type("@notwitter")
         cy.get('#submit').click()
     })
+    it("displays a name of the new contact", () => {
+		cy.get('#contact-list').should("contain", "Ali")
+    })
+    it("displays the phone number of the new contact", () => {
+		cy.get("#contact-list").should("contain", "707666999")
+	})
+
+
+
+
+
 })
