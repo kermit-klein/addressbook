5c4bad28a0d26a5ac2f9220e49dff71371c6580c
allezz <aerbay@gmail.com>
2020-03-31 19:06:58 +0200

    Add DOM Delete method,delete function delete method

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,84 +1,85 @@
-const  storage = window.localStorage
-const  renderContacts = () => {
-  const  contacts = JSON.parse(storage.getItem("contacts"))
-  
-  let  div = document.querySelector("#tbodylist")
-  if (contacts && contacts.length>0) {  // ASK FOR conditional
-	div.innerHTML = ''
-	const  ul = document.createElement("ul")
+const storage = window.localStorage
+const renderContacts = () => {
+    const contacts = JSON.parse(storage.getItem("contacts"))
+    let div = document.querySelector("#tbodylist")
+    if (contacts && contacts[0] != undefined) {
+        div.innerHTML = ''
+        contacts.forEach(contact => {
+            let tr = document.createElement("tr")
 
-	contacts.forEach(contact  => {
-		let tr = document.createElement("tr")
-
-		tr.innerHTML = `
+            tr.innerHTML = `
 		  <td>${contact.name}</td> 
 		  <td>${contact.email}</td> 
 		  <td>${contact.phone}</td> 
 		  <td>${contact.company}</td> 
 		  <td>${contact.twitter}</td> 
 		  <td>${contact.notes}</td>
-		  <button id="${contact.uID}" class="mini ui red button" onClick="delete_record()">Delete</button> 
+		  <button id="${contact.uID}" class="mini ui red button">Delete</button> 
 		`
-		div.appendChild(tr)
-		document.querySelector("#thead").style.display ="";
-	  })
-			
-	  
-	} else {
-	  document.querySelector("#thead").style.display ="none";
-	  div.innerHTML = '<p class="norecord">You have no contacts in your address book</p>'
-	}
+            div.appendChild(tr)
+            document.querySelector("#thead").style.display = "";
+        })
+    } else {
+        document.querySelector("#thead").style.display = "none";
+        div.innerHTML = '<p class="norecord">You have no contacts in your address book</p>'
+    }
+    document.querySelectorAll("button").forEach(button => {
+        button.addEventListener("click", function() {
+            var contacts = JSON.parse(window.localStorage.getItem("contacts"))
+            if (contacts && contacts[0] != undefined) {
+                for (let i = 0; i <= contacts.length - 1; i++) {
+                    if (this.id == contacts[i].uID) {
+                        contacts.splice(i, 1)
+                        storage.setItem('contacts', JSON.stringify(contacts))
+                        renderContacts()
+                    }
+                }
+            }
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	renderContacts()
-	const  contactForm = document.getElementById('new-contact-form')
-	const  toggleFormVisibilityButton = document.getElementById('add-contact')
-	contactForm.style.display = 'none'
-	
-	toggleFormVisibilityButton.addEventListener('click', () => {
-		if (contactForm.style.display === '') {
-			contactForm.style.display = 'none'
-		} else {
-			contactForm.style.display = ''
-		}
+    renderContacts()
+    const contactForm = document.getElementById('new-contact-form')
+    const toggleFormVisibilityButton = document.getElementById('add-contact')
+    contactForm.style.display = 'none'
+
+    toggleFormVisibilityButton.addEventListener('click', () => {
+        if (contactForm.style.display === '') {
+            contactForm.style.display = 'none'
+        } else {
+            contactForm.style.display = ''
+        }
 	})
-
-
-	contactForm.addEventListener('submit', event  => {
+	
+    contactForm.addEventListener('submit', event => {
         event.preventDefault()
-        
-		// 1. Read all the input fields and get their values
-		const { name, email, phone, company, notes, twitter } = contactForm.elements
-		const  contact = {
-			name:  name.value,
-			email:  email.value,
-			phone:  phone.value,
-			company:  company.value,
-			notes:  notes.value,
-			twitter:  twitter.value,
-			uID:Date.now()
-		}
 
-		let  contacts = JSON.parse(storage.getItem('contacts')) || []
+        // 1. Read all the input fields and get their values
+        const {
+            name,
+            email,
+            phone,
+            company,
+            notes,
+            twitter
+        } = contactForm.elements
+        const contact = {
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            company: company.value,
+            notes: notes.value,
+            twitter: twitter.value,
+            uID: Date.now()
+        }
 
-		contacts.push(contact)
-           
-		// 2. Save them to our storage
-		storage.setItem('contacts', JSON.stringify(contacts))
-		renderContacts()
-		contactForm.reset()
-   })
+        let contacts = JSON.parse(storage.getItem('contacts')) || []
+        contacts.push(contact)
+        // 2. Save them to our storage
+        storage.setItem('contacts', JSON.stringify(contacts))
+        renderContacts()
+        contactForm.reset()
+    })
 })
-
-var delete_record = () => {       // ASK for DOM
-	let contacts = JSON.parse(window.localStorage.getItem("contacts"))
-	for (i=0;i<=contacts.length-1;i++) {
-		if (event.srcElement.id==contacts[i].uID) {
-			contacts.splice(i,1)
-			storage.setItem('contacts', JSON.stringify(contacts))
-		    renderContacts()
-		}
-	}
-
-}
