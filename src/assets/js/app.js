82d52730c6c7bd5723348781a4b92f4dddc3b5ac
allezz <aerbay@gmail.com>
2020-04-01 13:44:42 +0200

    Fix bug Edit/delete button,hide them during edit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -86,8 +86,14 @@ var editfunc = () => {
             for (let i = 0; i <= contacts.length - 1; i++) {
                 if (this.id == contacts[i].uID) {
                     var contactedit = contacts[i]
-                    editdiv.innerHTML = `<form id="edit-contact-form"><div class="ui form"><div class="fields"><div class="field"><label for="name">Name</label><input id="editname" type="text" name="name" value="${contactedit.name}"></div><div class="field"><label for="email">E-mail</label><input id="editemail" type="email" name="email" value="${contactedit.email}"></div><div class="field"><label for="phone">Phone</label><input id="editphone" type="text" name="phone" value="${contactedit.phone}"></div><div class="field"><label for="company">Company</label><input id="editcompany" type="text" name="company" value="${contactedit.company}"></div><div class="field"><label for="twitter">Twitter</label><input id="edittwitter" type="text" name="twitter" value="${contactedit.twitter}"></div><div class="field"><label>Notes</label><input type="text" id="editnotes" name="notes" value="${contactedit.notes}"></div><button id="${contactedit.uID}" type="submit" value="Save contact" class="ui yellow button saveedit"><i class="icon user"></i>Save Edit</button></div></div></form>`
-
+					editdiv.innerHTML = `<form id="edit-contact-form"><div class="ui form"><div class="fields"><div class="field"><label for="name">Name</label><input id="editname" type="text" name="name" value="${contactedit.name}"></div><div class="field"><label for="email">E-mail</label><input id="editemail" type="email" name="email" value="${contactedit.email}"></div><div class="field"><label for="phone">Phone</label><input id="editphone" type="text" name="phone" value="${contactedit.phone}"></div><div class="field"><label for="company">Company</label><input id="editcompany" type="text" name="company" value="${contactedit.company}"></div><div class="field"><label for="twitter">Twitter</label><input id="edittwitter" type="text" name="twitter" value="${contactedit.twitter}"></div><div class="field"><label>Notes</label><input type="text" id="editnotes" name="notes" value="${contactedit.notes}"></div><button id="${contactedit.uID}" type="submit" value="Save contact" class="ui yellow button saveedit"><i class="icon user"></i>Save Edit</button></div></div></form>`
+					document.querySelectorAll(".delbut").forEach(button=>{
+						button.style.display="none"
+					})
+					document.querySelectorAll(".editbut").forEach(button=>{
+						button.style.display="none"
+					})
+                      
                     var editcontactForm = document.getElementById('edit-contact-form')
                     editcontactForm.addEventListener("submit", event => {
                         event.preventDefault()
